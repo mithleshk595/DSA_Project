cd087450903540b9dd30229fd4756e880c2c9ecc
Mithleshk595 <mithleshk0982@gmail.com>
2026-02-09 21:53:13 +0530

    Adde Stack.py

---
+++ DSA_PROJECT/Stack.py
@@ -0,0 +1,28 @@
+stack = []
+
+# Push 
+stack.append("A")
+stack.append("B")
+stack.append("C")
+
+print("Stack after pushing A, B, C:", stack)
+
+# peek 
+topElement = stack[-1]
+print("peek: ", topElement)
+
+#pop 
+topElement = stack.pop()
+print("pop: ", topElement)
+
+#stack and pop 
+print("Stack after pop: ", stack)
+
+# isEmpty
+isEmpty = not bool(stack)
+print("isEmpty: ", isEmpty)
+
+# Size
+print("Size: ",len(stack))
+
+
