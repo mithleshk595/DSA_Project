99c49e19411824dfb3ff92bf4742cdb73139d576
Mithleshk595 <mithleshk0982@gmail.com>
2026-02-16 21:26:16 +0530

    Added Selection_sort.py

---
+++ DSA_PROJECT/Selection_sort_with.py
@@ -0,0 +1,11 @@
+mylist = [64, 34, 24, 12, 22, 11, 90, 5]
+
+n = len(mylist)
+for i in range(n-1):
+    min_idx = i
+    for j in range(i+1, n):
+        if mylist[j] < mylist[min_idx]:
+            min_idx = j
+            mylist[i], mylist[min_idx] = mylist[min_idx], mylist[i]
+print(mylist)
+
