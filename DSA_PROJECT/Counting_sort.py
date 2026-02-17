794d42b55998229a489e4cd28432c0cb24011131
Mithleshk595 <mithleshk0982@gmail.com>
2026-02-17 22:09:47 +0530

    Added Counting_sort.py

---
+++ DSA_PROJECT/Counting_sort.py
@@ -0,0 +1,17 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    while len(arr) > 0:
+        num = arr.pop()
+        count[num] += 1
+
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr.append(i)
+            count[i] -= 1
+    
+    return arr
+mylist = [4, 2, 2, 8, 3, 3, 1, 0, 0, 7, 6, 5]
+mysortedlist = counting_sort(mylist)
+print(mysortedlist)
