18aecfd8d9d89540e0617613227a46292994635e
Mithleshk595 <mithleshk0982@gmail.com>
2026-02-23 21:18:27 +0530

    Added Last_occurrence.py

---
+++ DSA_PROJECT/Last_occurrence.py
@@ -0,0 +1,36 @@
+def last_occurrence(nums, target):
+    left = 0
+    right = len(nums) - 1
+    result = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            result = mid
+            left = mid + 1   # RIGHT side search continue
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
+
+
+# ---- Main Program ----
+nums = []
+
+for i in range(6):
+    num = int(input("Enter number: "))
+    nums.append(num)
+
+nums.sort()
+
+target = int(input("Enter number to search: "))
+
+index = last_occurrence(nums, target)
+
+if index != -1:
+    print("Last Occurrence at index:", index)
+else:
+    print("Number Not Found")
