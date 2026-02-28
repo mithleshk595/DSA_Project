126d8b9e1a40284e74c120beeacb44a118734e58
Mithleshk595 <mithleshk0982@gmail.com>
2026-02-28 21:18:39 +0530

    Added DSA_Hash.py

---
+++ DSA_PROJECT/DSA_Hash.py
@@ -0,0 +1,11 @@
+def hash_function(value):
+    sum_of_chars = 0
+    for char in value:
+        sum_of_chars += ord(char)
+
+    return sum_of_chars % 10
+
+print("'Bob' has hash code:",hash_function('Bob'))
+
+#Python
+
