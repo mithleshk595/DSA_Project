271d0b4d69125a4f54bc0dfbcf45e423a9e44d8d
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-19 22:16:07 +0530

    Added Basics.py

---
+++ DSA_PROJECT/Basic_DSA.py
@@ -0,0 +1,51 @@
+# list1 = [100, "hello", 12.22, "Mithlesh", True]
+
+# print(list1)
+# print(type(list1))
+# print(len(list1))
+# print(list1[0]) # Accesing the first elelment
+
+
+# list1 = [100, "hell", 12.22, "Mithlesh", True]
+
+# list1.append(0)
+# list1.append( [1, 2, 3]) 
+
+
+# print(list1)
+
+# list1 = [100, "hello", 12.22, "Mithlesh", True]
+
+# list2 = ( [1, 2 ,3] )
+
+# for i in list2:
+#     list1. append(i)
+
+# print(list1)
+
+# list1 = [100, "hello", 12.22, "Mithlesh", True]
+
+# list2 = [1, 2, 3]
+
+# list1.extend(list2)
+
+# print(list1)
+
+
+# list1 = [100, "hello", 12.22, "Mithlesh", True]
+
+# list3 = "Mithlesh"
+# list1.insert(2, "World")
+# print(list1)
+
+
+list1 = [100, "hello", 12.22, "Mithlesh", True,1,2,3]
+
+
+list1.remove(2) # Value Remove
+list1.pop(0) # Index Remove
+
+
+print(list1)
+
+
---
+++ DSA_PROJECT/Basics.DSA.py
@@ -0,0 +1,6 @@
+list1 = [100, "hello", 12.22, "Mithlesh", True]
+
+print(list1)
+print(type(list1))
+print(len(list1))
+print(list1[0]) # Accesing the first elelment
