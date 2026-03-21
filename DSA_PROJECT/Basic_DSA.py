57ebbcfdbbf75c43b51e9805883ec149d9bebeca
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-21 21:05:41 +0530

    Added inverse,py

--- DSA_PROJECT/Basic_DSA.py
+++ DSA_PROJECT/Basic_DSA.py
@@ -56,14 +56,6 @@
 # print(max(list1))
 
 
-# list1 = [3, 4, 5, 2, 3, 4, 5, 5, 6, 2, 2, 3, 4]
-
-# print(list1.count(2))
-# print(list1.count(3))
-
-# list1.sort(reverse=True)
-# print(list1)
-
 
 # list1.reverse()
 # print(list1)
---
+++ DSA_PROJECT/inverse.py
@@ -0,0 +1,7 @@
+list1 = [3, 4, 5, 2, 3, 4, 5, 5, 6, 2, 2, 3, 4]
+
+print(list1.count(2))
+print(list1.count(3))
+
+list1.sort(reverse=True)
+print(list1)
