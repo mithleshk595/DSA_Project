2f24be62d07e63a8cc1ba7141cb58ee81b82ba1b
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-24 21:22:49 +0530

    Added Tuple.py

---
+++ DSA_PROJECT/Tuple.py
@@ -0,0 +1,23 @@
+tuple = (100, "Hello", 12.22, "Mithlesh", True)
+
+print(len(tuple))
+print(type(tuple))
+print(tuple[0])
+print(list(tuple))
+print(tuple[-2])
+
+
+name = "Mithlesh"
+room = "B"
+Addrerss = "Sitamrhi, bihar, India"
+
+intro = """My name is Mithlesh,
+I am from bihar,
+I am living in Aligarh,
+I teach DSDA"""
+
+print(name, type(name))
+print(room, type(room))
+print( Addrerss, type(Addrerss))
+print(intro, type(intro))
+
