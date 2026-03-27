c983644441149591ee0ae6cfb9acba91841148f8
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-27 18:00:29 +0530

    Added isprime.py

--- DSA_PROJECT/Recursion.py
+++ DSA_PROJECT/Recursion.py
@@ -29,15 +29,30 @@
 #     return n*fact(n-1)
 # print(fact(5))
 
-def gcd(a, b):
-    #base case
-    if b==0:
-        return 
-     #recursive case
-    return gcd(b, a%b)
+# def gcd(a, b):
+#     #base case
+#     if b==0:
+#         return 
+#      #recursive case
+#     return gcd(b, a%b)
 
-print(gcd(21, 70))
-print(gcd(56, 98))
+# print(gcd(21, 70))
+# print(gcd(56, 98))
+
+def isPrime(n):
+    if n==0 or n==1:
+        return False
+    for i in range(2, n):
+        if n%i==0:
+            return False
+        return True
+print(isPrime(11))
+print(isPrime(15))
+print(isPrime(2))
+
+
+
+
 
 
 
