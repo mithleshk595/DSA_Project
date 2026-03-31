05c2dc8ba3793ae448d3e79454e755b9aee34428
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-31 22:08:14 +0530

    Added Marks.py

---
+++ DSA_PROJECT/Marks.py
@@ -0,0 +1,15 @@
+marks = int(input())
+
+# >= 90 Excellent
+# 70-90 Good
+#40-60 Fair
+# < 40 Poor
+
+if marks >=90:
+    print("Excellent")
+elif marks >=70:
+    print("Good")
+elif marks >=40:
+    print("fair")
+else:
+    print("Poor")
