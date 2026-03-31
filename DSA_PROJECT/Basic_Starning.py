58beb64e0c0ac8c287ae9bd9dd07816206d70003
Mithleshk595 <mithleshk0982@gmail.com>
2026-03-31 22:07:23 +0530

    Added Basic_Starning.py

---
+++ DSA_PROJECT/Basic_Starning.py
@@ -0,0 +1,117 @@
+# monday = 87
+# tuesday = 98
+
+# avg = (monday + tuesday) / 2
+# print(avg)
+
+# age = 12
+# height = 5.5
+# a = 4 + 2j
+
+# print(age, type(age))
+# print(height, type(height))
+# print(a, type(a))
+
+
+# s1 = "hello"
+# s2 = 'world'
+# s3 = "my name is mithlesh"
+
+# print(s1, type(s1))
+# print(s2, type(s2))
+# print(s3, type(s3))
+
+
+# first_name = input("Enter your firt name:")
+# last_name = input("Enter your last name:")
+
+# print(first_name, last_name)
+
+
+# age = int(input("Enter your age:"))
+# height = float(input("Enter your height:"))
+
+# print(age,height)
+
+
+# full_name = "mithlesh kumar"
+# age = 22
+# address = "Bihar"
+
+# print(full_name, end="&&")
+# print(age, end="&&")
+# print(address, end="_")
+
+# age = int(input())
+
+
+# if age >= 18:
+#     test = input()
+#     if test == "PASS":
+
+#        print("you are eligible for voting")
+
+#     else:
+         
+#         print("you are not eligible for voting")
+# else:
+#     print("Not eligible for votinh")
+
+
+
+
+# marks = int(input())
+
+# # >= 90 Excellent
+# # 70-90 Good
+# #40-60 Fair
+# # < 40 Poor
+
+# if marks >=90:
+#     print("Excellent")
+# elif marks >=70:
+#     print("Good")
+# elif marks >=40:
+#     print("fair")
+# else:
+#     print("Poor")
+
+
+# temp = int(input())
+
+# # 25-50 hot
+# # 10-24 cold
+# # <10 -> Extremly cold
+
+# if temp >= 25:
+#     print("Hot")
+# elif temp >= 10:
+#     print("Cold")
+# elif temp < 10:
+#     print("Extremly cold")
+
+
+
+# for i in range(1, 11):
+#     print(i, end="")
+
+# for i in range(20, 0):
+#     print(i, end="")
+
+# for i in range(1, 11, 2):
+#     print(i, end="")
+
+# for i in range(20, 0, -1):
+#     print(i, end="")
+
+
+def addTwoNumbers(a, b):
+    sum = a + b
+    return sum
+
+ans = addTwoNumbers(5, 10)
+print(ans)
+
+
+
+
