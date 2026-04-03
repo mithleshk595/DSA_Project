2023046a80d5ec19b7936b1225c8bdef391a16ca
Mithleshk595 <mithleshk0982@gmail.com>
2026-04-03 22:21:41 +0530

    Added DSA_Time_complexity.py

---
+++ DSA_PROJECT/DSA_Time_complexity.py
@@ -0,0 +1,68 @@
+# n = 10
+# x = 3
+# for i in range(n):
+#     if i == x:
+#         break
+#     print(i, end=" ")
+
+# 0(n)
+
+# n = 0
+
+# for i in range(n/2):
+#     print(i, end = " ")
+
+
+# for i in range(n/100):
+#     print(i, end= " ")
+
+
+# for i in range(n+100):
+#     print(i, end= " ")
+
+
+# for i in range(n*100):
+#     print(i, end= " ")
+
+# n = 10
+# m = 100
+# for i in range(n+m):
+#     print(i, end= " ")
+
+
+
+
+# for i in range(n):
+#     print(i, end=" ")
+
+# for i in range(n**2):
+#     print(i, end=" ")
+
+# for i in range(n):
+#     for j in range(n):
+#         print(i, j, end= " ")
+
+
+# n = 100
+
+# while n > 0:
+#     print(n, end= " ")
+#     n = n//2
+
+
+# n = 1
+
+# while n < 100:
+#     print(n, end= " ")
+#     n*=2
+
+
+i = 1
+n = 100
+
+while i <= n:
+    print(i, end= " ")
+    i*=2
+
+
+
