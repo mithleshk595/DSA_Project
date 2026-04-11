61f388dead6d5480fd52028d2668d1919c2fecc2
Mithleshk595 <mithleshk0982@gmail.com>
2026-04-11 22:24:42 +0530

    Added Reverse_string.py

---
+++ DSA_PROJECT/Reverse_string.py
@@ -0,0 +1,17 @@
+class solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        
+        """
+
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            i+=1
+            j-=1
+            
+
