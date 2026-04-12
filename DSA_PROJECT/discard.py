99055b469d9e311844e0380e654a24911945f6da
Mithleshk595 <mithleshk0982@gmail.com>
2026-04-12 21:40:09 +0530

    Added discard.py

---
+++ DSA_PROJECT/discard.py
@@ -0,0 +1,7 @@
+set1 = {2, 4, "Hello", 2, 3, "mithlesh"}
+
+set1.add("Hello")
+set1.add("world")
+
+set1.discard("Hello")
+set1.discard("2")
--- DSA_PROJECT/lenth_of_last_word.py
+++ DSA_PROJECT/lenth_of_last_word.py
@@ -28,13 +28,13 @@
 
 
 
-# set1 = {2, 4, "Hello", 2, 3, "mithlesh"}
+set1 = {2, 4, "Hello", 2, 3, "mithlesh"}
 
-# set1.add("Hello")
-# set1.add("world")
+set1.add("Hello")
+set1.add("world")
 
-# set1.discard("Hello")
-# set1.discard("2")
+set1.discard("Hello")
+set1.discard("2")
 
 
 # print(set1)
