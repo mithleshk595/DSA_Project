995bb8670476a225825655bfdc7e9e667d97ce0d
Mithleshk595 <mithleshk0982@gmail.com>
2026-04-14 18:17:20 +0530

    Added intersection.py

--- DSA_PROJECT/dict_key.py
+++ DSA_PROJECT/dict_key.py
@@ -1,12 +1,40 @@
-dict1 = {1: "Mithlesh", 2: "Kumar", 3: "singh"}
+# dict1 = {1: "Mithlesh", 2: "Kumar", 3: "singh"}
 
-print(dict1.keys())
-for key,value in dict1.items():
-    print(key, value)
+# print(dict1.keys())
+# for key,value in dict1.items():
+#     print(key, value)
 
-print(dict1.items())
-for key in dict1.keys():
-    print(key, dict1[key])
+# print(dict1.items())
+# for key in dict1.keys():
+#     print(key, dict1[key])
     
+
+
+# list1 = ["mITHLESH", "ABHINAV", "SAIF", "RAHUL", "MANISH", "MITHLESH", "ABHINAV"]
+
+
+# FREQ = {}
+
+# for name in list1:
+#     if name is not FREQ:
+#         FREQ[name] = 1
+#     else:
+#         FREQ[name] += 1
+
+# print(FREQ)
+
+s = "Physcis wallah skills"
+
+FREQ = {}
+
+for name in s:
+    if name is not FREQ:
+        FREQ[name] = 1
+    else:
+        FREQ[name] += 1
+
+print(FREQ)
+
+
 
 
---
+++ DSA_PROJECT/intersection_of_two.py
@@ -0,0 +1,8 @@
+class solution:
+    def intersection(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        return list(set1.intersection(set2))
+    
+    
