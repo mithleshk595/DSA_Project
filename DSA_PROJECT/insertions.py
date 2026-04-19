0712061e8d330c719bad87d985004d467a46fad2
Mithleshk595 <mithleshk0982@gmail.com>
2026-04-19 16:45:53 +0530

    Added insertion.py

---
+++ DSA_PROJECT/insertions.py
@@ -0,0 +1,14 @@
+# insertion sort
+
+class solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+
+        for i in range(1, n):
+            key = nums[i]
+            j = i-1
+            while j>=0 and nums[j]>key:
+                nums[j+1] = nums[j]
+                j-=1
+            nums[j+1] = key
+        return nums
