fb7680fa186325ccbafc96a4e3f4f933d51c6810
Mithleshk595 <mithleshk0982@gmail.com>
2026-05-07 21:55:04 +0530

    Added intersection.py

---
+++ DSA_PROJECT/intersection.py
@@ -0,0 +1,20 @@
+class solution:
+    def getIntersectionNode(self, headA, headB):
+        p1 = headA
+        p2 = headB
+        count = 0
+
+        while True:
+            if p1 == p2:
+                return p1
+            if p1 == None:
+                p1 = headB
+            if p2 == None:
+                p2 = headA
+            p1 = p1.next
+            p2 = p2,next
+            count+=1
+            if count > 10000:
+                return None
+            
+            
